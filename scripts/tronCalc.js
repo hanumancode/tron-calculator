9885c0af099b9175fec5de6f271b3516a8f55693
Gareth Jones <garethjones@cybertron.local>
2018-06-09 22:48:03 +0100

    update from ES5 to ES6

--- scripts/tronCalc.js
+++ scripts/tronCalc.js
@@ -1,9 +1,12 @@
-var trx = 3.5;
-var trxUsdBought = 7500;
-var trxUsdNow = 12000;
+// var is ES5
+// const and let is ES6
 
-var invested = trx * trxUsdBought;
-var current = trx * trxUsdNow;
+const trx = 3.5;
+const trxUsdBought = 7500;
+const trxUsdNow = 12000;
+
+const invested = trx * trxUsdBought;
+const current = trx * trxUsdNow;
 
 console.log("amount of trx bought in USD value:", '$' + invested);
 console.log("trx value now", '$' + current);
