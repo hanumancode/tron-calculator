9807b6a93ea1ee736a15141a24543e776bf2ea55
Gareth Jones <garethjones@cybertron.local>
2018-06-09 22:30:18 +0100

    JS variable updates for TRX invested and current values

--- scripts/tronCalc.js
+++ scripts/tronCalc.js
@@ -1,6 +1,10 @@
 var trx = 3.5;
-var trxUsdBought = 75000;
+var trxUsdBought = 7500;
+var trxUsdNow = 12000;
 
 var invested = trx * trxUsdBought;
+var current = trx * trxUsdNow;
 
-console.log(invested);
+console.log("amount of trx bought in USD value:", '$' + invested);
+console.log("trx value now", '$' + current);
+
