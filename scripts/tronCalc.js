0942da02729b0a907c7f48e1f02e0adc517ae223
Gareth Jones <garethjones@cybertron.local>
2018-06-10 14:07:00 +0100

    added conditional statements for deciding whether user is at a profit, loss, or breakeven

--- scripts/tronCalc.js
+++ scripts/tronCalc.js
@@ -2,7 +2,7 @@
 // const and let is ES6
 
 const trx = 3.5;
-const trxUsdBought = 7500;
+const trxUsdBought = 1000;
 const trxUsdNow = 12000;
 
 const invested = trx * trxUsdBought;
@@ -11,8 +11,18 @@ const current = trx * trxUsdNow;
 const profit = current - invested;
 const growth = (profit / invested) * 100;
 
+/*
 console.log('amount of trx bought in USD value:', '$' + invested);
 console.log('trx value now', '$' + current);
 console.log('profit = ', '$' + profit);
 console.log('growth = ', growth + '%');
+*/
+
+if (profit > 0) { 
+  console.log('You made a profit of ' + profit + ' ' + 'USD');
+} else if (profit < 0) {
+  console.log('You made a loss of ' + profit + ' ' + 'USD');
+} else {
+  console.log('You\'re at breakeven');
+}
 
